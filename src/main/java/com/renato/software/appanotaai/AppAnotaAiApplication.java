9220f7b31e33ed12478f13b7ccf7e0d0b6c07f66
renatoredes <renatoredes@live.com>
2024-01-28 23:50:35 -0300

    setup

---
+++ src/main/java/com/renato/software/appanotaai/AppAnotaAiApplication.java
@@ -0,0 +1,13 @@
+package com.renato.software.appanotaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAnotaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAnotaAiApplication.class, args);
+	}
+
+}
